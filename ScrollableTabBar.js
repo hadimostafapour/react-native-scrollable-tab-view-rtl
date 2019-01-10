b528546942eae327f417be03417a2999b655dcbc
hadimostafapour <design.zerzem@gmail.com>
2019-01-10 23:19:18 +0330

    Update based on main repo commits

--- ScrollableTabBar.js
+++ ScrollableTabBar.js
@@ -160,7 +160,7 @@ const ScrollableTabBar = createReactClass({
     measureTab(page, event) {
         const {x, width, height,} = event.nativeEvent.layout;
         this._tabsMeasurements[page] = {left: x, right: x + width, width, height,};
-        this.updateView({value: this.props.scrollValue._value,});
+        this.updateView({value: this.props.scrollValue.__getValue(), });
     },
 
     render() {
@@ -233,12 +233,12 @@ const ScrollableTabBar = createReactClass({
             width = WINDOW_WIDTH;
         }
         this.setState({_containerWidth: width,});
-        this.updateView({value: this.props.scrollValue._value,});
+        this.updateView({value: this.props.scrollValue.__getValue(), });
     },
 
     onContainerLayout(e) {
         this._containerMeasurements = e.nativeEvent.layout;
-        this.updateView({value: this.props.scrollValue._value,});
+        this.updateView({value: this.props.scrollValue.__getValue(), });
     },
 });
 
